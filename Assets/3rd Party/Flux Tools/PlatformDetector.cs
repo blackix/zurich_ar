e5c13d32515756699de5b1e8a3a8f3eb8516ea3f
blackix <43524847+blackix@users.noreply.github.com>
2022-01-28 18:33:21 +0100

    support also iphone and ipad in parallel

---
+++ Assets/3rd Party/Flux Tools/PlatformDetector.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlatformDetector : MonoBehaviour
+{
+    public UnityEvent OnPlatformAndroidDetected;
+    public UnityEvent OnPlatformIphoneDetected;
+    public UnityEvent OnPlatformIpadDetected;
+    public UnityEvent OnPlatformPcDetected;
+
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer ||
+            Application.platform == RuntimePlatform.OSXPlayer)
+        {
+
+
+            bool deviceIsIphoneX = UnityEngine.iOS.Device.generation.ToString().Contains("iPhone");
+            if (deviceIsIphoneX)
+            {
+                // Do something for iPhone X
+                OnPlatformIphoneDetected.Invoke();
+            }
+
+            bool deviceIsIpad = UnityEngine.iOS.Device.generation.ToString().Contains("iPad");
+            if (deviceIsIpad)
+            {
+                // Do something for iPad
+                OnPlatformIpadDetected.Invoke();
+            }
+
+            /*doesnt work
+            var identifier = SystemInfo.deviceModel;
+            if (identifier.StartsWith("iPhone"))
+            {
+                // iPhone logic
+                OnPlatformIphoneDetected.Invoke();
+            }
+            else if (identifier.StartsWith("iPad"))
+            {
+                // iPad logic
+                OnPlatformIpadDetected.Invoke();
+            }
+            else
+            {
+                // Mac logic?
+            }
+            */
+            
+        }
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            OnPlatformAndroidDetected.Invoke();
+        }
+
+        if (Application.platform == RuntimePlatform.WindowsPlayer ||
+            Application.platform == RuntimePlatform.WindowsEditor )
+        {
+            OnPlatformPcDetected.Invoke();
+        }
+    }
+}
